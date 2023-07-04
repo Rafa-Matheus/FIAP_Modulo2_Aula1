00d2c0423cfafdfc1332f708657aa6824477a37a
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-04 18:30:16 -0300

    Create Editora.cs

---
+++ FIAP_Modulo2_Aula1/Editora.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Modulo2_Aula1
+{
+    class Editora
+    {
+        public string Nome;
+        public string Site;
+        public string Telefone;
+        public void ExibirDados() { }
+    }
+}
