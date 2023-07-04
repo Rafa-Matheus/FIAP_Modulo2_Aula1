45c304f0cb27d58caf070ad3b0f8afb69bba2538
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-04 18:30:23 -0300

    Create Livro.cs

---
+++ FIAP_Modulo2_Aula1/Livro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Modulo2_Aula1
+{
+    public class Livro
+    {
+        public string Nome;
+        public string Resumo;
+        public double Valor;
+        public int Paginas;
+        public string Autor;
+
+        public void ExibirDados()
+        {
+            String Titulo = "DETALHES DO LIVRO";
+            Console.WriteLine(Titulo);
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Autor: " + Autor);
+            Console.WriteLine("Resumo: " + Resumo);
+            Console.WriteLine($"Valor: R$ {Valor.ToString("F2")}");
+            Console.WriteLine("Páginas: " + Paginas);
+        }
+    }
+}
