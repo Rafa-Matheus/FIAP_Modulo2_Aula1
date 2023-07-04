4732b34d2144dfe68865068d75dc15713bde5584
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-04 18:32:41 -0300

    Updates and adjustments

--- FIAP_Modulo2_Aula1/Program.cs
+++ FIAP_Modulo2_Aula1/Program.cs
@@ -10,6 +10,24 @@ namespace FIAP_Modulo2_Aula1
     {
         static void Main(string[] args)
         {
+            #region Testes com a finalidade de aprendizado
+            /*
+            Editora edit = new Editora();
+            edit.Nome = "Rambo Geraldo";
+
+            Livro livro1 = new Livro();
+            livro1.Nome = "Memória de um Zé Ninguém - Edição de Luxo";
+            livro1.Resumo = "Viagem ao fim do próprio mundo";
+            livro1.Valor = 120.00;
+            livro1.Paginas = 752;
+            livro1.Autor = "Rambo Geraldo";
+
+            livro1.ExibirDados();
+            Console.ReadKey();
+            */
+            #endregion
+
+
         }
     }
 }
