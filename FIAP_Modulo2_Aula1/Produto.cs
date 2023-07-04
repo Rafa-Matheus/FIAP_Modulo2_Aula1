49fd2ea9861c7e339c39084e4ef79de7850b8300
Rafa-Matheus <108595172+Rafa-Matheus@users.noreply.github.com>
2023-07-04 18:30:28 -0300

    Create Produto.cs

---
+++ FIAP_Modulo2_Aula1/Produto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Modulo2_Aula1
+{
+    class Produto
+    {
+        public string Nome;
+        public string Descricao;
+        public string Genero;
+        public string Foto;
+        public bool Status;
+
+        public string ExibirDados()
+        {
+            return " ";
+        }    
+    }
+}
